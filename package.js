c536299fd7ff06ade5b23ffa96b8b25b24917fa7
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2016-06-05 02:21:47 +0200

    Add package files
    Add package.json, package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  documentation: 'README.md',
+  git: 'https://github.com/meteor-activeroute/core.git',
+  name: 'activeroute:core',
+  version: '0.0.1',
+  summary: 'The core utilities for active-route',
+});
+
+Package.onUse(function onUse(api) {
+  api.versionsFrom('1.3.2.4');
+  api.use([
+    'check',
+    'ecmascript',
+    'reactive-dict',
+  ]);
+});
