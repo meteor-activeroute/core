ec55b9184ac827ee32759f80b8c84f2b7ec83d59
Simon Fridlund <simon@fridlund.email>
2017-01-19 08:49:27 +0100

    Update eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@
 module.exports = {
   env: {
     browser: true,
+    es6: true,
     meteor: true,
     node: true,
+    'shared-node-browser': true,
   },
   extends: [
     'airbnb-base',
@@ -18,13 +20,24 @@ module.exports = {
   rules: {
     'import/extensions': ['error', 'never'],
     'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': ['error',
-      {
-        ignore: ['^meteor/'],
-      },
-    ],
+    'import/no-unresolved': ['error', {
+      ignore: ['^meteor/'],
+    }],
+    'max-len': ['error', 120, 2, {
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreUrls: true,
+    }],
     'max-params': ['warn', 1],
+    'no-param-reassign': ['warn', {
+      props: true,
+    }],
     'no-use-extend-native/no-use-extend-native': 'error',
+    'prefer-arrow-callback': ['error', {
+      allowNamedFunctions: true,
+    }],
     'prefer-object-spread/prefer-object-spread': 'error',
     'sort-keys': 'warn',
     'sort-vars': 'warn',
